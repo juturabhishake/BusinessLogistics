1498c614c5a96f5f0592ca6d759bbf60288053e7
abhishakejutur <abhishakejutur@gmail.com>
2025-02-26 16:07:53 +0530

    Update page.jsx

--- app/settings/print/LCL/page.jsx
+++ app/settings/print/LCL/page.jsx
@@ -421,7 +421,7 @@ const QuotationTable = () => {
     tableBody.push([
       { content: "", styles: { halign: "center" } },
       { content: "Total Destination Charges", styles: { halign: "center" } },
-      { content: currency, styles: { halign: "center" } },
+      { content: "INR", styles: { halign: "center" } },
       ...totalC.map(val => ({ content: val, styles: { halign: "center" } })),
       { content: "", styles: { halign: "center" } }
   ]);
